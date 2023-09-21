b5f465779df91062dafb9717020cc83c5393c2b4
MKING301 <33675411+MKing301@users.noreply.github.com>
2023-09-20 20:44:26 -0400

    Register manufacturer model to admin

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, MapLocation, Area
+from .models import User, MapLocation, Area, Manufacturer
 
 
 admin.site.register(User)
 admin.site.register(MapLocation)
 admin.site.register(Area)
+admin.site.register(Manufacturer)
