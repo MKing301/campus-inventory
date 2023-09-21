5b98955f09d85d03b6d17b906506f26a4998f8bb
MKING301 <33675411+MKing301@users.noreply.github.com>
2023-09-20 20:36:40 -0400

    Register area model to admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User, MapLocation
+from .models import User, MapLocation, Area
 
 
 admin.site.register(User)
 admin.site.register(MapLocation)
+admin.site.register(Area)
