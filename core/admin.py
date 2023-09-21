a6266e8f6629077ade705497a6b85f80bd98f95f
MKING301 <33675411+MKing301@users.noreply.github.com>
2023-09-20 21:07:03 -0400

    Register inventory item and approval list to admin

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from .models import User, MapLocation, Area, Manufacturer
+from .models import (
+    User,
+    MapLocation,
+    Area,
+    Manufacturer,
+    InventoryItem,
+    ApprovalList
+    )
 
 
 admin.site.register(User)
 admin.site.register(MapLocation)
 admin.site.register(Area)
 admin.site.register(Manufacturer)
+admin.site.register(InventoryItem)
+admin.site.register(ApprovalList)
