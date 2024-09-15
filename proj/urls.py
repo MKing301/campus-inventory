73e85d05947c798fc56d17e4098bf80337a4700c
MKING301 <33675411+MKing301@users.noreply.github.com>
2024-09-15 09:59:41 -0400

    Add urp pattern for favorite icon

--- proj/urls.py
+++ proj/urls.py
@@ -1,8 +1,15 @@
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',  include('core.urls')),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
 ]
